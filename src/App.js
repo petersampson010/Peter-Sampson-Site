200bf0cea90e460a74bc25890b48e6d7752cd745
Peter Sampson <petersampson010@gmail.com>
2021-04-28 13:44:14 +0100

    added jest and react-native skills

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './App.scss';
 import Profile from './PageSections/Profile';
 import Projects from './PageSections/Projects';
